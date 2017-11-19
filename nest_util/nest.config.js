e9ca2c5b923fd7a436c0e40fc287767c699bb46f
Kay Christensen <kaychristensen@gmail.com>
2017-11-18 21:03:43 -0800

    Move Nest utilities to root.

---
+++ nest_util/nest.config.js
@@ -0,0 +1,9 @@
+const credentials = {
+  PROD_ID: '0c3af9dd-40fd-468d-9bb5-fb47007b1f69',
+  PROD_SECRET: 'N38M9DYYFRXn58sbrEaEBlahN',
+  AUTH_URL: 'https://home.nest.com/login/oauth2?client_id=0c3af9dd-40fd-468d-9bb5-fb47007b1f69&state=STATE',
+  PIN: 'E3DU8BXL',
+  ACCESS_TOKEN: 'c.IUEtN3Th7MVFnuvSRdlFCSqKfT0OIvs1T1dFNSDDPRobns6HOO8AP8kGq8ihncQrpXDg7PPhee2OPw2bvA07Zjz3dWzwni8qconIH3rSM97pad27mfvs3IMX8vV9FIftyLVAHXEEpM5x6HIr'
+};
+
+module.exports = credentials;
