23f16adcc1470c830b11df404f6973f080a3c439
Kay Christensen <kaychristensen@gmail.com>
2017-11-18 20:47:50 -0800

    Import Nest API variables.

--- client/lib/nest.js
+++ client/lib/nest.js
@@ -1,24 +1,30 @@
 const request = require('superagent');
+const { PROD_ID, PROD_SECRET, PIN } = require('./nest.config');
 
 const nest = () => {
-  request.post('https://api.home.nest.com/oauth2/access_token')
+  request
+    .post('https://api.home.nest.com/oauth2/access_token')
     .query({
-      client_id: '0c3af9dd-40fd-468d-9bb5-fb47007b1f69',
-      client_secret: 'N38M9DYYFRXn58sbrEaEBlahN',
+      client_id: PROD_ID,
+      client_secret: PROD_SECRET,
       grant_type: 'authorization_code',
-      code: '9XNUS4H6'
+      code: PIN
     })
     .then(function(res) {
       console.log('access_token', res.body.access_token);
       bearer(res.body.access_token);
     })
     .catch(function(err) {
-      console.error(err);
+      console.log(
+        'Error in access token post:',
+        err.response.res.req.socket._httpMessage.res.text
+      );
     });
 };
 
-const bearer = (token) => {
-  request.get('https://developer-api.nest.com')
+const bearer = token => {
+  request
+    .get('https://developer-api.nest.com')
     .accept('application/json')
     .set('Authorization', `Bearer ${token}`)
     .query()
@@ -26,9 +32,10 @@ const bearer = (token) => {
       if (err) {
         console.log('Error in GET', err);
       } else {
-        console.log('res is', res);
+        console.log('Response:', res);
       }
     });
 };
 
+// TODO: call from App in componentDidMount
 nest();
