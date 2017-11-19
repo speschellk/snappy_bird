821487f4e361e118d7ff533062788c4c102e0b34
Kay Christensen <kaychristensen@gmail.com>
2017-11-19 14:52:40 -0800

    Add takeSnapshot utility.

--- nest_util/cameraUtilities.js
+++ nest_util/cameraUtilities.js
@@ -1,11 +1,9 @@
 const request = require('superagent');
-const { PROD_ID, PROD_SECRET, PIN, ACCESS_TOKEN } = require('./nest.config');
 
-const getCameraInfo = (token, callback) => {
+const getCameraInfo = (callback) => {
   request
     .get('https://developer-api.nest.com')
-    .accept('application/json')
-    .set('Authorization', token)
+    .set('Authorization', process.env.ACCESS_TOKEN)
     .end((err, { body: { devices: { cameras } } }) => {
       if (err) {
         console.error('Error in GET', err);
@@ -17,20 +15,11 @@ const getCameraInfo = (token, callback) => {
     });
 };
 
-const viewPublicURL = callback => {
-  request
-    .get(public_share_url)
-    .then(res => callback(res))
-    .catch(err => console.log(err));
-};
-
 const takeSnapshot = () => {
-  request
-    .get(snapshot_url)
+  return request
+    .get('https://www.dropcam.com/api/wwn.get_snapshot/CjZsLU5sbzZHNWt0a1lxcTJxNHUyRTBwTDFEMGNIUVBER1JZa1pBR1dGSndnYTl5aFJaRnN0ZFESFmg1OVVYSGxSbVNLSy00Z2lwRHM2b2caNlJrSE1QeDlCa2FCVkQ3U08wUlNJNXlQdGFqaGQ0Zm8xWnR6NVNnS3U4Z3JCeFBzU3BCcFR0dw?auth=276TLaPo8WuHVOLRUugP_j7Vs_HL8xN70wm1AtKt2Q2WFfI0AzRYyZsYwsKYJu9ZzVLqzAw6HHV89FkVFnRAvqC5VqLGRpBw4H5o48BYxyAW2-jLP0z15_LrEacU-Pz4BOFH8pNjB0Gl99qnFVPnJtz0TCS_aIrkRULRfb8Ok42Blg-4-8hKX2f69--94z5B55i_PMVHkBCnS0')
     .then(res => callback(res))
     .catch(err => console.log(err));
 };
 
-getCameraInfo(ACCESS_TOKEN, info => {
-  console.log('info is:', info);
-});
+module.exports = { getCameraInfo, viewLivestream, takeSnapshot };
