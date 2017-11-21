984067cf7f5da223a14d6486ee8e6f3093cb78e6
Kay Christensen <kaychristensen@gmail.com>
2017-11-20 17:32:52 -0800

    Add axios.

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Video from './Video';
 import SnapButton from './SnapButton';
 import SnapPanel from './SnapPanel';
-import request from 'superagent';
+import axios from 'axios';
 
 class App extends Component {
   state = {
@@ -10,10 +10,11 @@ class App extends Component {
   };
 
   handleSnapshot = () => {
-    request
+    axios
       .get('/snapshot')
-      .then((img) => {
-        this.state.pics.push(img);
+      .then(({data}) => {
+        console.log('then:', data)
+        this.state.pics.push(data);
         this.setState({ pics: this.state.pics });
       })
       .catch(err => console.log(err));
@@ -23,9 +24,7 @@ class App extends Component {
     return (
       <div>
         <Video url={'https://video.nest.com/embedded/live/VaM9CN'} />
-        <SnapButton
-          handleSnapshot={this.handleSnapshot}
-        />
+        <SnapButton handleSnapshot={this.handleSnapshot} />
         <SnapPanel pics={this.state.pics} />
       </div>
     );
