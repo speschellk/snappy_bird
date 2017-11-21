e17ac0aae7076cd081eacb290345b9d806093c7f
Kay Christensen <kaychristensen@gmail.com>
2017-11-20 17:34:23 -0800

    Format snapshot.

--- client/Snapshot.jsx
+++ client/Snapshot.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Snapshot = props => (
-  <img src={props.img}/>
-);
+const Snapshot = props => {
+  return (
+    <span className="snapshot-container">
+      <img src={props.filepath} alt="Hummingbird" height="100" width="200" />
+    </span>
+  );
+};
 
 export default Snapshot;
