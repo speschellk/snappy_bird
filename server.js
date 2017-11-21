1fc61c4bd23d3bf2fdac8050f6517acd73b439ce
Kay Christensen <kaychristensen@gmail.com>
2017-11-20 17:31:26 -0800

    Update /snapshot method.

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const path = require('path');
-const request = require('superagent');
 const fs = require('fs');
+const crypto = require('crypto');
+const axios = require('axios');
 
 const port = 3000;
 
 const app = express();
 app.use(express.static(path.join(__dirname), { etag: true }));
 app.get('/snapshot', (req, res) => {
-  console.log('server snapshot')
-  request(
-    'https://www.dropcam.com/api/wwn.get_snapshot/CjZsLU5sbzZHNWt0a1lxcTJxNHUyRTBwTDFEMGNIUVBER1JZa1pBR1dGSndnYTl5aFJaRnN0ZFESFmg1OVVYSGxSbVNLSy00Z2lwRHM2b2caNlJrSE1QeDlCa2FCVkQ3U08wUlNJNXlQdGFqaGQ0Zm8xWnR6NVNnS3U4Z3JCeFBzU3BCcFR0dw?auth=276TLaPo8WuHVOLRUugP_j7Vs_HL8xN70wm1AtKt2Q2WFfI0AzRYyZsYwsKYJu9ZzVLqzAw6HHV89FkVFnRAvqC5VqLGRpBw4H5o48BYxyAW2-jLP0z15_LrEacU-Pz4BOFH8pNjB0Gl99qnFVPnJtz0TCS_aIrkRULRfb8Ok42Blg-4-8hKX2f69--94z5B55i_PMVHkBCnS0'
-  )
-    .pipe(fs.createWriteStream('bird.png'))
-    res.send('./bird.png')
+  let fileName = `bird${Date.now()}.jpeg`;
+
+  axios({
+    method: 'get',
+    url: `https://www.dropcam.com/api/wwn.get_snapshot/${process.env
+      .SNAP_ID}?auth=${process.env.SNAP_AUTH}`,
+    responseType: 'stream'
+  })
+    .then(response => {
+      response.data.pipe(fs.createWriteStream(fileName));
+      response.data.on('end', () => {
+        res.send(fileName);
+      });
+    })
+    .catch(err => {
+      console.log('Error getting snapshot:', err);
+    });
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
